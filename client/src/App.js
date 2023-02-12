4efa94f5f4360bbeeb8d054ea9ddc5cf9240a918
Bhargavi Nayak <nayakbhargavi01@gmail.com>
2023-02-12 07:53:47 +0530

    add listusers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import InputUser from './components/InputUser';
+import ListUser from './components/ListUser';
 
 function App() {
   return (
     <div className="App">
       <InputUser />
+      <ListUser />
     </div>
   );
 }
---
+++ client/src/components/ListUser.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react'
+import EditUser from './EditUser'
+
+const ListUser = () => {
+
+    const [users, setUsers] = useState([]);
+
+    const deleteUser = async (id) => {
+        try {
+
+            const deleteUser = await fetch(`http://localhost:5000/users/${id}`, {
+                method: "DELETE"
+            })
+
+            setUsers(users.filter((user) => user.user_id !== id))
+            
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+
+    const getUsers = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/users')
+            const jsonData = await response.json()
+            setUsers(jsonData)
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+  return (
+      <div>
+          <table class="table mt-5 text-center">
+              <thead>
+                  <tr>
+                      <th>Username</th>
+                      <th>Email</th>
+                      <th>Edit</th>
+                      <th>Delete</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  {
+                      users.map(user => (
+                          <tr key={user.user_id}>
+                              <td>{user.username}</td>
+                              <td>{user.email}</td>
+                              <td>
+                                  <EditUser user={user.user_id} />
+                              </td>
+                              <td>
+                                  <button onClick={() => deleteUser(user.user_id)}>
+                                      Delete
+                                  </button>
+                              </td>
+                          </tr>
+                      ))
+                  }
+                  
+              </tbody>
+          </table>
+    </div>
+  )
+}
+
+export default ListUser
